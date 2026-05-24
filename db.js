7c3d38dde1f6f9e3370642443c4b096c2a0e4e57
petrunicuta8-code <petrunicuta8@gmail.com>
2026-05-25 00:51:57 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"discord"
+    host: "HOSTUL_TAU",
+    user: "USERUL_TAU",
+    password: "PAROLA_TA",
+    database: "DATABASE_TA",
+    port: 3306
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log("DB ERROR:", err);
+    } else {
+        console.log("🔥 DB CONNECTED");
+    }
 });
 
 module.exports = db;
